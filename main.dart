5e42a1f78298cda9205006b628b346895d22c24a
BMB <badamasi41@gmail.com>
2022-07-13 00:16:12 -1200

    Added comments to program

--- main.dart
+++ main.dart
@@ -1,22 +1,28 @@
 import 'dart:io';
 void main(){
-  stdout.writeln("Enter a String to be reversed: ");
-   var name = stdin.readLineSync();
-  name = name.toString();
-  String Revname = '';
-  var reverse = name.split('');
-  String reversed = reverse.toString();
-  var u = reversed.length;
-  var hm = [];
+  String Revname = ''; // AN EMPTY STRING VARIABLE
+  var hm = []; // AN EMPTY LIST OF ARRAY
+  
+  stdout.writeln("Enter a String to be reversed: "); // TAKES INPUT FROM USER 
+  var name = stdin.readLineSync(); // STORE USERS INPUT TO REVERSE
+  name = name.toString(); // INPUT DATA TYPE CHANGED TO STRING
+ 
+  var reverse = name.split(''); //VARIABLE TO STORE USER INPUT AFTER SPLIT
+  String reversed = reverse.toString(); //STRING VARIABLE TO STORE AFTER CHANGING SPLITTED VARIABLE TO STRING
+  var u = reversed.length; // VARIABLE TO STORE LENGTH OF INPUT
+  
+  /// FOR LOOP TO ACCESS LENGTH OF INPUT AND ASSIGN TO AN ARRAY IF CONDITION IS MET (USING ALGORITHM)
   for (var e = 1; e<reversed.length; e+=3){
  hm  += [reversed[reversed.length-(u-e)]]; }
   var i= hm.length-1;
   var j = hm.length;
+
+/// FOR LOOP TO CONCATENATE INTO EMPTY VARIABLE CHARACTERS FROM ARRAY
   for ( i ; i<j; i--,j--){
 if (j==1){j--;}
 Revname += hm[i];
 if (j==0){break;}
 
   }
-print('Your reversed string is: $Revname');
+print('Your reversed string is: $Revname'); // DISPLAY OUTPUT OF REVERSED STRING
 }
