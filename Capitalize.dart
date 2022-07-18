8df46d4952d80572c1d03530e1f9c60a06470eb8
BMB <badamasi41@gmail.com>
2022-07-18 11:39:04 -1200

    Created Full Program

--- Capitalize.dart
+++ Capitalize.dart
@@ -3,19 +3,21 @@ import 'dart:io';
 void main() {
   var letters = 'abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ ';
   var output = '';
-  stdout.writeln('Enter Word(s) to Capitalize: ');
+  stdout.writeln(
+      'Enter Word(s) to Capitalize: N / B  T H I S   P R O G R A M   I G N O R E   N U M B E R S ');
   var input = stdin.readLineSync().toString();
   for (var i = 0; i < input.length; i++) {
     for (var j = 0; j < letters.length; j++) {
       if (input[i] == letters[j]) {
-        var a = letters.indexOf(letters[j]) + 27;
-        for (var x = 27; x < 54; x++) {
+        int a = (letters.indexOf(letters[j]) + letters.length / 2).toInt();
+        for (var x = letters.length / 2; x < letters.length; x++) {
           if (letters.indexOf(letters[j]) == x) {
             output += letters[j];
             break;
           }
         }
-        if (letters.indexOf(letters[j]) < 27) output += letters[a];
+        if (letters.indexOf(letters[j]) < letters.length / 2)
+          output += letters[a];
       }
     }
   }
