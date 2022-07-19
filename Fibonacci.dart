2074fe9744128cd028e19186da3ebf7bb0b562e8
BMB <badamasi41@gmail.com>
2022-07-19 11:33:10 -1200

    Added Exception

--- Fibonacci.dart
+++ Fibonacci.dart
@@ -1,25 +1,32 @@
 import 'dart:io';
 
 void main() {
-  stdout.writeln('Enter Length Of Fibonacci Sequence: ');
-  var inn = stdin.readLineSync().toString();
-  print('=============================================');
-  var No = int.parse(inn);
-  var sum = 0;
-  var tester;
-  var calc = '1';
-  for (var x = 0; inn.isNotEmpty; x++) {
-    calc += ' ' + sum.toString();
-    tester = calc.split(' ');
-    for (var y = 0; y < x; y++) {
-      tester.removeAt(0);
+  try {
+    stdout.writeln('Enter Length Of Fibonacci Sequence: ');
+    var inn = stdin.readLineSync().toString();
+    if (inn.contains(RegExp('[A-Za-z]'))) {
+      throw ('No Character(s) Input');
     }
-    var m1 = int.parse(tester[0]);
-    var m2 = int.parse(tester[1]);
-    sum = m1 + m2;
-    sum = sum;
-    if (x == 0) print(x);
-    if (sum > No) break;
-    print(sum);
+    print('=============================================');
+    var No = int.parse(inn);
+    var sum = 0;
+    var tester;
+    var calc = '1';
+    for (var x = 0; inn.isNotEmpty; x++) {
+      calc += ' ' + sum.toString();
+      tester = calc.split(' ');
+      for (var y = 0; y < x; y++) {
+        tester.removeAt(0);
+      }
+      var m1 = int.parse(tester[0]);
+      var m2 = int.parse(tester[1]);
+      sum = m1 + m2;
+      sum = sum;
+      if (x == 0) print(x);
+      if (sum > No) break;
+      print(sum);
+    }
+  } catch (e) {
+    print(e);
   }
 }
